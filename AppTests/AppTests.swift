c0abd6d12da13244cd8db700ef094481b1c66c9b
Paul Zabelin <paulz@users.noreply.github.com>
2022-05-28 18:52:45 -0700

    add popularity badge

--- AppTests/AppTests.swift
+++ AppTests/AppTests.swift
@@ -5,5 +5,6 @@ import ViewSnapshotTesting
 class AppTests: XCTestCase {
     func testPreviews() throws {
         verifySnapshot(ContentView_Previews.self)
+        verifySnapshot(PopularityBadge_Previews.self)
     }
 }

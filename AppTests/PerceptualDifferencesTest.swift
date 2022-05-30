71a5ca918ca1ea1119b15ec8f5b0aca1a11a939a
Paul Zabelin <paulz@users.noreply.github.com>
2022-05-30 16:22:39 -0700

    add PerceptualDifferencesTest

---
+++ AppTests/PerceptualDifferencesTest.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import ExampleApp
+@testable import ViewSnapshotTesting
+
+class PerceptualDifferencesTest: XCTestCase {
+    func testNeglibleDifferences() {
+        let intelUrl = folderUrl().appendingPathComponent("ToggleView-intel.png")        
+    }
+}

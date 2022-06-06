98a3f9b3487026abf64ab385d66b9e09286b4b5c
Paul Zabelin <paulz@users.noreply.github.com>
2022-06-06 03:03:19 -0700

    add SVG and PDF examples

--- AppTests/SnapshotTests.swift
+++ AppTests/SnapshotTests.swift
@@ -1,6 +1,7 @@
 import XCTest
 @testable import ExampleApp
 import ViewSnapshotTesting
+import SwiftUI
 
 class SnapshotTests: XCTestCase {
     func testPreviews() throws {
@@ -10,5 +11,21 @@ class SnapshotTests: XCTestCase {
         verifySnapshot(LayeringShadowsView_Previews.self)
         verifySnapshot(RainbowGlowView_Previews.self, colorAccuracy: 0.047)
         verifySnapshot(ToggleView_Previews.self, colorAccuracy: 0.032)
+    }
+    
+    func testXcodeSVGassetNotMatchingPDf() {
+        /**
+         https://en.wikipedia.org/wiki/Test_card
+            
+            converted to PDF using: https://cloudconvert.com/svg-to-pdf
+         
+            demonstrates that SVG rendered incorrectly in Xcode Assets, missing central cicrcular pattern
+            while PDF rendered perfectly and has smaller file size
+            
+         https://commons.wikimedia.org/wiki/File:Philips_PM5544.svg
+         new versión by ebnz, CC BY-SA 3.0 <http://creativecommons.org/licenses/by-sa/3.0/>, via Wikimedia Commons
+         */
+        verifySnapshot(Image("Philips_PM5544_PDF"), "tv-set-test-pattern.pdf")
+        verifySnapshot(Image("Philips_PM5544_SVG"), "tv-set-test-pattern.svg")
     }
 }

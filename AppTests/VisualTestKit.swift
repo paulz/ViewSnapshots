592c0a97d169c68125b80628371f36563678fbe2
Paul Zabelin <paulz@users.noreply.github.com>
2022-06-08 22:56:51 -0700

    update extension to use API without underscore

--- AppTests/VisualTestKit.swift
+++ AppTests/VisualTestKit.swift
@@ -11,9 +11,16 @@ extension XCTestCase {
     func VTKAssertView(_ view: UIView, name: String, file: String = #file, line: UInt = #line) {
         VTKAssertView(view, name: name, decorations: [], file: file, line: line)
     }
-    func VTKAssertView(_ view: UIView, name: String, decorations: VTKAssertIDDecorations, file: String = #file, line: UInt = #line) {
-        let identifier = VTKID(name, decorations)
-        __VTKAssertView(view, identifier, self, file, line)
+    func VTKAssertView(_ view: UIView,
+                       name: String,
+                       decorations: VTKAssertIDDecorations,
+                       file: String = #file,
+                       line: UInt = #line) {
+        visualTestKitAssert.assertView(
+            view,
+            identifier: VTKID(name, decorations),
+            filePath: file,
+            lineNumber: line
+        )
     }
-
 }

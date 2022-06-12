73e9b14844d2d7308f1a65518f8fa273be5f0710
Paul Zabelin <paulz@users.noreply.github.com>
2022-06-12 01:42:13 -0700

    pass CI_TEAM_ID to tests
    log environment variables
    
    Squashed commit of the following:
    
    commit b73a19eb46edf8e56ee447ed72e2a4dc28a597a0
    Author: Paul Zabelin <paulz@users.noreply.github.com>
    Date:   Sun Jun 12 01:08:07 2022 -0700
    
        remove exit
    
    commit 9d577d44abeb0a6877ab603bd765f6b59bb40db4
    Author: Paul Zabelin <paulz@users.noreply.github.com>
    Date:   Sun Jun 12 00:51:36 2022 -0700
    
        pass CI_TEAM_ID to tests
    
        log environment variables

--- AppTests/Principal.swift
+++ AppTests/Principal.swift
@@ -4,18 +4,30 @@ import ViewSnapshotTesting
 class Principal: NSObject {
     override init() {
         super.init()
+        logEnvironment()
         configureSnapshots()
     }
 }
 
-func runningOnXcodeCloud() -> Bool {
-    ProcessInfo.processInfo.environment["CI_TEAM_ID"] != nil
+func logEnvironment() {
+    NSLog("ProcessInfo.processInfo.environment")
+    ProcessInfo.processInfo.environment.forEach {
+        NSLog($0.key + "=" + $0.value)
+    }
 }
 
 func configureSnapshots() {
     if runningOnXcodeCloud() {
+        NSLog("""
+              Configure Snapshots to load from Resouces
+              since XCode Cloud runs tests on differenet environment from clone
+              """)
         getSnapshotsFromResources()
     }
+}
+
+func runningOnXcodeCloud() -> Bool {
+    ProcessInfo.processInfo.environment["CI_TEAM_ID"]?.isEmpty == false
 }
 
 func getSnapshotsFromResources() {

eccf20c75e5c5201f898c8b3ae14c74f881b0aa6
Paul Zabelin <paulz@users.noreply.github.com>
2022-06-13 00:03:55 -0700

    test stability of XML

--- AppTests/CoreAnimationArchiveTests.swift
+++ AppTests/CoreAnimationArchiveTests.swift
@@ -3,12 +3,34 @@ import XCTest
 @testable import ViewSnapshotTesting
 
 class CoreAnimationArchiveTests: XCTestCase {
-    func testCoreAnimationArchive() {
-        let archiveData = try! inWindowView(RainbowGlowView_Previews.previews) {
-            try coreAnimationArchive(view: $0)
-        }
+    func testCoreAnimationArchiveIsSmallAndStableBeforeRendered() {
+        let archiveData = getArchive(shouldRender: false)
         XCTAssertEqual(archiveData.count, 1325)
         XCTAssertEqual(archiveData.sha1string(), "037af7bb2d90b9307640150ac31962dd5c7b3112")
+    }
+    
+    func getArchive(shouldRender: Bool) -> Data {
+        try! inWindowView(RainbowGlowView_Previews.previews) { view -> Data in
+            if shouldRender {
+                _ = view.renderLayerAsBitmap()
+            }
+            return try coreAnimationArchive(view: view)
+        }
+    }
+
+    func testCoreAnimationArchiveAfterRenderIsNotStable() {
+        let archiveData1 = getArchive(shouldRender: true)
+        let archiveData2 = getArchive(shouldRender: true)
+        XCTAssertEqual(archiveData1.count, 19316)
+        XCTAssertNotEqual(archiveData1, archiveData2)
+    }
+    
+    func testArchiveXMLAddsInstability() throws {
+        let caarData = getArchive(shouldRender: true)
+        var format: PropertyListSerialization.PropertyListFormat = .xml
+        let plist1 = try! PropertyListSerialization.propertyList(from: caarData, format: &format)
+        let plist2 = try! PropertyListSerialization.propertyList(from: caarData, format: &format)
+        XCTAssertNotEqual(plist1 as? NSDictionary, plist2 as? NSDictionary)
     }
 }
 
@@ -17,20 +39,4 @@ func coreAnimationArchive(view: UIView) throws -> Data {
         withRootObject: ["rootLayer": view.layer],
         requiringSecureCoding: false
     )
-}
-
-import CommonCrypto
-
-extension Data {
-    func sha1string() -> String {
-        sha1().map { String(format: "%02hhx", $0) }.joined()
-    }
-
-    func sha1() -> [UInt8] {
-        var digest = [UInt8](repeating: 0, count:Int(CC_SHA1_DIGEST_LENGTH))
-        withUnsafeBytes {
-            _ = CC_SHA1($0.baseAddress, CC_LONG(count), &digest)
-        }
-        return digest
-    }
 }
---
+++ AppTests/Data+Extensions.swift
@@ -0,0 +1,16 @@
+import Foundation
+import CommonCrypto
+
+extension Data {
+    func sha1string() -> String {
+        sha1().map { String(format: "%02hhx", $0) }.joined()
+    }
+
+    func sha1() -> [UInt8] {
+        var digest = [UInt8](repeating: 0, count:Int(CC_SHA1_DIGEST_LENGTH))
+        withUnsafeBytes {
+            _ = CC_SHA1($0.baseAddress, CC_LONG(count), &digest)
+        }
+        return digest
+    }
+}

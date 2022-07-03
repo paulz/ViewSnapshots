a0eb2e8898a3ed100328e47b173945b736f7c824
Paul Zabelin <paulz@users.noreply.github.com>
2022-07-03 15:00:39 -0700

    add favorite view to tests

---
+++ ExampleApp/FavoriteView.swift
@@ -0,0 +1,36 @@
+//
+//  FavoriteView.swift
+//  Experiments
+//
+//  Created by Paul Zabelin on 5/18/22.
+//
+
+import SwiftUI
+import PreviewGroup
+
+/// https://www.hackingwithswift.com/articles/226/5-steps-to-better-swiftui-views
+struct FavoriteView: View {
+    var body: some View {
+        Image(systemName: "heart.fill")
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.red)
+            .clipShape(Circle())
+            .overlay(
+                Circle()
+                    .strokeBorder(Color.white, lineWidth: 2)
+            )
+    }
+}
+
+
+public struct FavoriteView_Previews: PreviewProvider {
+    public static var previews: some View {
+        PreviewGroup {
+            FavoriteView()
+            FavoriteView()
+                .background(Color.white)
+        }
+        .previewLayout(.sizeThatFits)
+    }
+}

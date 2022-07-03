e2d4f4014721654385b72d41ebc5e73247d14027
Paul Zabelin <paulz@users.noreply.github.com>
2022-07-02 17:54:41 -0700

    fix: complex view mismatch in CoreAnimation Bundle (#14)
    * fix: https://github.com/paulz/ViewSnapshots/issues/13
    
    by increasing color tolerance to default 2%
    
    * show VisualTestKit version info
    
    * fix: https://github.com/paulz/ViewSnapshots/issues/15
    
    add more customization to complex view
    
    * replace expected when different sizes

--- ExampleApp/ComplexView.swift
+++ ExampleApp/ComplexView.swift
@@ -4,16 +4,21 @@ import SwiftUI
 ///  https://betterprogramming.pub/an-overview-of-built-in-swiftui-views-374ea7549bf9
 struct ComplexView: View {
     @State private var toggle = false
-    @State private var sliderValue = 15.0
-    @State private var stepperValue = 7.0
-    @State private var date = Date(timeIntervalSinceReferenceDate: 87654321)
+    @State private var sliderValue = 12.0
+    @State private var stepperValue = 2
+    @State var date = Date()
     @State private var color: Color = .red
 
     var body: some View {
         VStack {
-            Image(systemName: "cart")
+            Image(systemName: "cloud.sun.rain.fill")
+                .renderingMode(.original)
                         .resizable()
-                        .frame(width: 40, height: 40)
+                        .font(.largeTitle)
+                        .background(Color.black)
+                        .clipShape(Circle())
+                        .frame(width: 60, height: 60)
+                        .padding()
             Toggle("Toggle", isOn: $toggle)
             HStack {
                 Slider(value: $sliderValue, in: 0...30.0, minimumValueLabel: Text("0"), maximumValueLabel: Text("30"), label: {
@@ -21,22 +26,38 @@ struct ComplexView: View {
                 })
                 Text("\(Int(sliderValue))")
             }
-            Stepper("Stepper: \(Int(stepperValue))", value: $stepperValue)
-            Picker(selection: .constant(2), label: Text("Picker"), content: {
-                Text("1").tag(1)
-                Text("2").tag(2)
-                Text("3").tag(3)
-            })
+            Stepper("Stepper: \(stepperValue)", value: $stepperValue)
+            Picker(selection: $stepperValue,
+                   label: Text("Picker \(stepperValue)")) {
+                Text("♥️ - Hearts").tag(1)
+                Text("♣️ - Clubs").tag(2)
+                Text("♠️ - Spades").tag(3)
+                Text("♦️ - Diamonds").tag(4)
+            }
+                   .pickerStyle(.wheel)
+                   .padding(-20)
             DatePicker("Date:", selection: $date)
             ColorPicker("Color:", selection: $color)
-
         }.padding()
     }
 }
 
 struct ComplexView_Previews: PreviewProvider {
     static var previews: some View {
-        ComplexView()
+        ComplexView(date: .sample)
             .previewLayout(.sizeThatFits)
     }
+}
+
+extension Date {
+    static var sample: Date! = Calendar.current
+        .date(
+            from: DateComponents(
+                year: 2022,
+                month: 7,
+                day: 2,
+                hour: 17,
+                minute: 06
+            )
+        )
 }
--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,3 +2,9 @@
 
 system_profiler SPHardwareDataType
 system_profiler SPDisplaysDataType
+
+echo VisualTestKit version.plist
+cat /System/Library/PrivateFrameworks/VisualTestKit.framework/Resources/version.plist
+
+echo VisualTestKit Info.plist
+cat /System/Library/PrivateFrameworks/VisualTestKit.framework/Resources/Info.plist

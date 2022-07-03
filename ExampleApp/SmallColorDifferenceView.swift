d6f997cd98ec534805e840b01eb4276b2925293d
Paul Zabelin <paulz@users.noreply.github.com>
2022-07-03 15:48:32 -0700

    show percent in color difference

--- ExampleApp/SmallColorDifferenceView.swift
+++ ExampleApp/SmallColorDifferenceView.swift
@@ -1,17 +1,27 @@
 import SwiftUI
+import PreviewGroup
 
 struct SmallColorDifferenceView: View {
+    var difference = 0.01
     var body: some View {
-        VStack(spacing: 0) {
-            Rectangle().foregroundColor(Color(red: 0, green: 1, blue: 0))
-            Rectangle().foregroundColor(Color(red: 0, green: 0.95, blue: 0))
+        VStack {
+            Text("Color difference \(Int(difference * 100))%").font(.headline)
+            HStack(spacing: 0) {
+                ForEach((1...10), id: \.self) { _ in
+                    Rectangle().foregroundColor(Color(red: 0, green: 1, blue: 0))
+                    Rectangle().foregroundColor(Color(red: 0, green: 1 - difference, blue: 0))
+                }
+            }.frame(width: 100, height: 100)
         }
     }
 }
 
 struct SmallColorDifferenceView_Previews: PreviewProvider {
     static var previews: some View {
-        SmallColorDifferenceView()
-            .previewLayout(.sizeThatFits)
+        PreviewGroup {
+            SmallColorDifferenceView(difference: 0.01)
+            SmallColorDifferenceView(difference: 0.03)
+        }
+        .previewLayout(.sizeThatFits)
     }
 }
